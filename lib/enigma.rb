fb1883f44b665bc996a86abfd5d4a22899701d8b
Anthony Gomes <code@tonygom.es>
2020-03-02 10:13:21 -0700

    Save worl

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,15 @@
 class Enigma
+  def encrypt(message, key = nil, date = nil)
+    generate_key(key)
+    generate_date(date)
 
+    {
+      encryption: encrypt_message(message, key, date),
+      key: final_key,
+      date: final_date
+    }
+  end
+
+  def decrypt(ciphertext, key = nil, date = nil)
+  end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -10,4 +10,15 @@ class EnigmaTest < MiniTest::Test
   def test_it_exists
     assert_instance_of Enigma, @enigma
   end
+
+  def test_it_can_encrypt_message_without_parameters
+    message = 'Hello World!'
+    assert @enigma.encrypt(message)
+  end
+
+  def test_it_can_decrypt_message_without_parameters
+    skip
+    cyphertext = 'keder ohulw'
+    assert @enigma.decrypt(cyphertext)
+  end
 end
